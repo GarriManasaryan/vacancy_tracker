92928dd31e968760975cd92281ce8b30e0a4ac40
garri_manasaryan <garri.manasaryan@bostongene.com>
2024-02-18 03:20:20 +0400

    Clean commons

--- backend/config/dependencies.py
+++ backend/config/dependencies.py
@@ -11,13 +11,3 @@ def validate_salary_creation_request(
             status_code=404, detail="Both min and max values cant be empty"
         )
     return creation_request
-
-
-def validate_vacancy_creation_request(
-        creation_request: SalaryCreationRequest,
-) -> SalaryCreationRequest:
-    if creation_request.min is None and creation_request.max is None:
-        raise HTTPException(
-            status_code=404, detail="Both min and max values cant be empty"
-        )
-    return creation_request
